29843ac35d7b1d8f84168a0ebc8cc4a3a733c10f
huanndhe172325 <Huanndhe172325@fpt.edu.vn>
2024-05-21 13:41:04 +0700

    U DATE

---
+++ src/java/DAL/DBContext.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author FPT
+ */
+public class DBContext {
+    protected Connection connect;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName= FUSWAPSHAREFREE";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connect = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+
+}
+}
