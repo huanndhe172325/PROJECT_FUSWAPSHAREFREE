63d67d44b25a83a48d0975d369832d5b9c2c05f5
huanndhe172325 <Huanndhe172325@fpt.edu.vn>
2024-05-23 00:03:05 +0700

    Update Logout.java

--- src/java/Controllers/LogOut/Logout.java
+++ src/java/Controllers/LogOut/Logout.java
@@ -54,6 +54,7 @@ public class Logout extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        processRequest(request, response);
     } 
 
     /** 
