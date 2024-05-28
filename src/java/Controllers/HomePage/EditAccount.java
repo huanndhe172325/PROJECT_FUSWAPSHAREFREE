47d112885e96cabbdbcf6dfe029c96d834ba1bf2
anhnhhe163978 <144357804+Anhnhhe163978@users.noreply.github.com>
2024-05-28 14:48:23 +0700

    SignUp
    SignUp

---
+++ src/java/Controllers/HomePage/EditAccount.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controllers.HomePage;
+
+import DAL.DAOSignup;
+import Model.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "EditAccount", urlPatterns = {"/EditAccount"})
+public class EditAccount extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+      User u = new User();
+       u.setUserID(Integer.parseInt(request.getParameter("id")));
+        u.setEmail(request.getParameter("email"));
+                u.setPhone(request.getParameter("phone"));
+
+                u.setPassWord(request.getParameter("pass"));
+                
+                u.setUserName(request.getParameter("user"));
+                u.setFull_Name(request.getParameter("fname"));
+                u.setDistrict(request.getParameter("district"));
+                u.setCommune(request.getParameter("commune"));
+                u.setStreetNumber(request.getParameter("Strnumbet"));
+                u.setPoint(Integer.parseInt(request.getParameter("point")));
+                u.setRoleID(2);
+                u.setStatusID(Integer.parseInt(request.getParameter("status")));
+                DAOSignup dbs = new DAOSignup();
+                dbs.updateAccount(u);
+                response.sendRedirect("ManageAcount");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
