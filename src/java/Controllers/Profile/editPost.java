2960508999dec96fbd3cc8b21f1a6c62878a02ed
huanndhe172325 <Huanndhe172325@fpt.edu.vn>
2024-06-16 21:55:06 +0700

    update edit post

--- src/java/Controllers/Profile/editPost.java
+++ src/java/Controllers/Profile/editPost.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controllers.Profile;
 
+import DAL.DAOManagePost;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -13,40 +13,44 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 import java.io.InputStream;
+import java.util.Collection;
 
 /**
  *
  * @author FPT
  */
 public class editPost extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet editPost</title>");  
+            out.println("<title>Servlet editPost</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet editPost at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet editPost at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,12 +58,13 @@ public class editPost extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -67,19 +72,21 @@ public class editPost extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        String title = request.getParameter("title");
-        String description = request.getParameter("description");
-
+            throws ServletException, IOException {
+        String title = request.getParameter("titleEdit");
+        String description = request.getParameter("descriptionEdit");
+        String instructions = request.getParameter("instructionsEdit");
+        String expiresDate_raw = request.getParameter("expiresDateEdit");
         try (PrintWriter out = response.getWriter()) {
-            out.print("successful" + title);
+            out.print(title + description + instructions + expiresDate_raw);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
